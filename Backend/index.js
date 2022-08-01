5b5d8635445dd3edc8683b11df9d119da4983c11
masoomarubab <67429673+masoomarubab@users.noreply.github.com>
2022-08-01 18:52:49 +0500

    ChatApp Uploading First Time

---
+++ Backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const { Server } = require('socket.io')
+const http = require('http')
+const cors = require('cors')
+
+const app = express();
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log(socket.id); 
+    socket.on("joinRoom",room =>  socket.join(room))
+    socket.on("newMessage" , ({newMessage , room}) => {
+        io.in(room).emit("latestMessage" , newMessage)
+    })
+});
+
+app.get('/', (req, res) => {
+    res.send("Socket start")
+})
+
+server.listen(8000, () => console.log('App started'))
